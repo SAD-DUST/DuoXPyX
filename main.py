664896cde6465813f87e4c477551b4a9a2e2088c
SAD DUST <90087346+SAD-DUST@users.noreply.github.com>
2024-05-24 22:03:10 +0700

    Update main.py

--- main.py
+++ main.py
@@ -97,6 +97,19 @@ headers = {
     'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36',
 }
 
+try:
+    token = config.get('User', 'TOKEN')
+    lessons = config.get('User', 'LESSONS')
+except:
+    create_config()
+
+# Configure headers for futher request
+headers = {
+    'Content-Type': 'application/json',
+    'Authorization': f'Bearer {token}',
+    'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36',
+}
+
 # Token processing 
 try:
     jwt_token = token.split('.')[1]
